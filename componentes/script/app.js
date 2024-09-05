cf512950a11520c900e8d3ee7cc4954a40591fce
l44ver <laurentbmvasconcelos@hotmail.com>
2024-09-05 17:09:19 -0300

    conserto do bug na home

--- componentes/script/app.js
+++ componentes/script/app.js
@@ -1,13 +1,17 @@
 function pesquisar() {
     let section = document.getElementById("container_pesquisa_resultado")
     console.log(section);
+    
+    section.classList.add("container_pesquisa_resultado")
 
     let resultados = ""
     for (let dado of dados) {
     resultados += `
+    <div class = "item-resultado">
     <h2>${dado.titulo}</h2>
     <p class="container_resultado_descricao">${dado.descricao}</p>
     <a href="${dado.link}" target="_blank">Saiba mais...</a>
+    </div>
 `
 }
 section.innerHTML = resultados
