337a3ac93dfd425192fb392f44735d9c0bd88d04
l44ver <laurentbmvasconcelos@hotmail.com>
2024-09-05 12:40:20 -0300

    aula 4

--- componentes/script/app.js
+++ componentes/script/app.js
@@ -1,1 +1,14 @@
-console.log(dados)
+function pesquisar() {
+    let section = document.getElementById("container_pesquisa_resultado")
+    console.log(section);
+
+    let resultados = ""
+    for (let dado of dados) {
+    resultados += `
+    <h2>${dado.titulo}</h2>
+    <p class="container_resultado_descricao">${dado.descricao}</p>
+    <a href="${dado.link}" target="_blank">Saiba mais...</a>
+`
+}
+section.innerHTML = resultados
+}
--- componentes/script/dados.js
+++ componentes/script/dados.js
@@ -1,21 +1,21 @@
 let dados = [
     {
-        nome : "Coelho Holland Lop",
+        titulo : "Coelho Holland Lop",
         descricao : "De origem holandesa, são uma clássica raça de minicoelho. Eles pesam entre 1 e 2 kg, são calmos e muito procurados por famílias com crianças. Suas orelhas são largas e caídas, e o pelo forma uma coroa no topo da cabeça. Com patas curtas e corpo largo, parecem sempre estar encolhidos. São brincalhões e carinhosos, mas não muito agitados.",
         link : "https://bini-the-bunny.fandom.com/wiki/Holland_Lop",
     },
     {
-        nome : "Coelho Netherland Dwarf (Anão)",
+        titulo : "Coelho Netherland Dwarf (Anão)",
         descricao : "Sua altura pode variar, desde a fase filhote até a adulta, de 15 a 25 cm, sendo considerado o menor minicoelho que pode ser encontrado no planeta. O Netherland Dwarf tem um temperamento bastante ativo, principalmente quando ainda são filhotes.",
         link : "https://www.petz.com.br/blog/pets/coelho-anao/",
     },
     {
-        nome : "Coelho Leão",
+        titulo : "Coelho Leão",
         descricao : "O coelho leão é uma raça pequena com uma pelagem fofa e uma juba distinta ao redor da cabeça, semelhante a uma leoa. Eles têm um temperamento dócil e amigável, tornando-os ótimos animais de estimação.",
         link : "https://www.petz.com.br/blog/coelhos/coelho-leao/",
     },
     {
-        nome : "Coelho Rex",
+        titulo : "Coelho Rex",
         descricao : "O coelho Rex é conhecido por sua pelagem curta e macia, com uma textura aveludada única. Seu corpo é robusto e compacto, e ele tem um temperamento amigável e curioso, ideal para a companhia.",
         link : "https://www.petlove.com.br/ceolho-rex/r?srsltid=AfmBOorIq_PTynIOf9TPlG-RpbK2nbBRytsnhbZ3U7yJn1d_uQNgl40q",
     },
